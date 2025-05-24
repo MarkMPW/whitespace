954c61301564c9c317a1afedd2c985144d0979de
MarkMPW <watsapol.ping@gmail.com>
2025-05-24 22:12:53 +0700

    fix: style circle position

--- components/WorkManagement.tsx
+++ components/WorkManagement.tsx
@@ -5,31 +5,48 @@ import Image from "next/image";
 
 const WorkManagement = () => {
   return (
-    <div className='md:p-8 px-3 h-screen flex items-center justify-center'>
-      <div className='flex justify-between items-center w-full'>
-        <div className='relative flex items-center justify-center w-[583px] h-[583px]'>
-          <div className='absolute border-2 rounded-full size-[440px]'></div>
-          <div className='absolute border-2 rounded-full size-[250px]'></div>
+    <div className="md:p-8 px-3 h-screen flex items-center justify-center">
+      <div className="flex justify-between items-center w-full">
+        <div className="relative flex items-center justify-center w-[583px] h-[583px] border-2">
+          <div className="absolute border-2 border-dashed rounded-full size-[500px] -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+            <div className="absolute size-[400px] rotate-0 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+              <div className="absolute bg-[#FFDC4D] size-12 rounded-full"></div>
+            </div>
+            <div className="absolute size-[400px] rotate-80 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+              <div className='absolute bg-[#37A3FF] size-12 rounded-full'></div>
+            </div>
+            <div className="absolute size-[400px] rotate-150 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+              <div className='absolute bg-[#00CA75] size-12 rounded-full'></div>
+            </div>
+            <div className="absolute size-[400px] rotate-255 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+              <div className='absolute bg-[#37A3FF] size-12 rounded-full'></div>
+            </div>
+          </div>
 
-          <div className='absolute top-[140px] border-2 rounded-full size-[50px] bg-[#00CA75]'></div>
-          <div className='absolute bottom-[140px] border-2 rounded-full size-[50px] bg-[#FFBF60]'></div>
-          <div className='absolute left-[140px] border-2 rounded-full size-[50px] bg-[#37A3FF]'></div>
-          <div className='absolute right-[140px] border-2 rounded-full size-[50px] bg-[#37A3FF]'></div>
+          <div className="absolute border-2 border-dashed rounded-full size-[300px] -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 ">
+            <div className="absolute size-[255px] rotate-45 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+              <div className="absolute bg-[#00CA75] size-12 rounded-full"></div>
+            </div>
+              <div className="absolute size-[255px] rotate-135 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+              <div className='absolute bg-[#37A3FF] size-12 rounded-full'></div>
+            </div>
+            <div className="absolute size-[255px] rotate-225 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+              <div className='absolute bg-[#FFBF60] size-12 rounded-full'></div>
+            </div>
+            <div className="absolute size-[255px] rotate-315 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+              <div className='absolute bg-[#37A3FF] size-12 rounded-full'></div>
+            </div>
+          </div>
 
-          <div className='absolute top-[50px] left-[50px] border-2 rounded-full size-[40px] bg-[#FF6B6B]'></div>
-          <div className='absolute bottom-[50px] border-2 rounded-full size-[40px] bg-[#4ECDC4]'></div>
-          <div className='absolute left-[50px] border-2 rounded-full size-[40px] bg-[#FFD93D]'></div>
-          <div className='absolute right-[50px] border-2 rounded-full size-[40px] bg-[#FFD93D]'></div>
-
-          <Image 
+          <Image
             src="/icons/white-space-icon.png"
             alt="Whitepace"
             width={100}
             height={100}
-            className="relative z-10"
+            className="z-10"
           />
         </div>
-        <div className='text-center md:text-left max-w-[630px]'>
+        <div className="text-center md:text-left max-w-[630px]">
           <h1 className="text-3xl font-bold mb-4">Work together</h1>
           <p className="text-lg mb-6">
             With whitepace, share your notes with your colleagues and
